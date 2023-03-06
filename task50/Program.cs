d711ed9ea858d2b787eb973e03b0ec679a786081
Anastasia Kotlyarova <mumminastya@gmail.com>
2023-03-06 23:27:05 +0300

    HomeWork_7

---
+++ task50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает
+//значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void ReturnNumb(int[,] matrix, int i, int j)
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+    if (i >= rowCount || j >= colCount)
+    {
+        Console.WriteLine("Такого элемента в массиве нет!");
+    }
+    else
+    {
+        Console.WriteLine(matrix[i,j]);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+System.Console.Write("Введите номер строки: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите номер столбца: ");
+int column = Convert.ToInt32(value: Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(5, 5);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReturnNumb(matrix, row, column);
---
+++ task52/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    Random rnd = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(0, 10);
+            System.Console.Write(matrix[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    return matrix;
+}
+
+double[] AverageColumn(int[,] matrix)
+{     
+    int colCount = matrix.GetLength(0);
+    int rowCount = matrix.GetLength(1);
+    double[] averagemassive = new double[colCount];
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        int summ = 0;
+        for (int i = 0; i < rowCount; i++)
+        {
+            summ = summ + matrix[i,j];
+        }
+        averagemassive[j] = Math.Round(1.0 * summ/rowCount, 1);
+    }
+    return averagemassive;  
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillMatrixWithRandom(row, column);
+double[] averagemassive = AverageColumn(matrix);
+System.Console.WriteLine();
+Console.Write("Среднее арифметическое каждого столбца: ");
+Console.Write(string.Join("; ", averagemassive));
